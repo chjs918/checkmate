856f9b03c1484960977fbd123c06c2ceccb5b90d
dbfla8772 <dbfla5527@naver.com>
2021-08-08 23:50:09 +0900

    로그인뷰 url 수정

--- account/views.py
+++ account/views.py
@@ -14,7 +14,7 @@ def login_view(request):
             if user is not None:
                 login(request, user)
 
-        return redirect("home")
+        return redirect("main")
     else:
         form = AuthenticationForm()
         return render(request, 'login.html', {'form':form})
