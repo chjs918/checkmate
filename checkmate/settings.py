74bdc316a77c0515f0267e22443d16b39c9c5286
alexiskim66 <ehpark10t@gmail.com>
2021-08-12 00:00:34 +0900

    heroku 배포

--- checkmate/settings.py
+++ checkmate/settings.py
@@ -148,8 +148,6 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
 AWS_ACCESS_KEY_ID = 'AKIA3VUHWCDS4DBOG2XY'
 
 AWS_SECRET_ACCESS_KEY = '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'
@@ -162,8 +160,3 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 AWS_S3_CUSTOM_DOMAIN = 'chekmate-bucket.s3.ap-northeast-2.amazonaws.com'
 
-# DATA_UPLOAD_MAX_MEMORY_SIZE = 1024000000 # value in bytes 1GB here
-# FILE_UPLOAD_MAX_MEMORY_SIZE = 1024000000
-
-# DEFAULT_FILE_STORAGE = 'checkmate.storages.S3DefaultStorage'
-# STATICFILES_STORAGE = 'checkmate.storages.S3StaticStorage'
